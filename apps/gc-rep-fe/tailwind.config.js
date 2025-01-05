ad12e1d50e908230fd061964f35b650a6eb70638
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-05 16:02:39 +0000

    Added flowbite and using tailwindcss/defaultTheme

--- apps/gc-rep-fe/tailwind.config.js
+++ apps/gc-rep-fe/tailwind.config.js
@@ -1,14 +1,23 @@
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['InterVarialbe', ...defaultTheme.fontFamily.sans] 
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
