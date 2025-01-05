acefd8f41c0df97fa1f95be2454c7d3293a77430
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-05 16:04:08 +0000

    configured a basic (empty) front end page

--- apps/gc-rep-fe/src/app/app.component.ts
+++ apps/gc-rep-fe/src/app/app.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
-    imports: [NxWelcomeComponent, RouterModule],
-    selector: 'gc-rep-fe-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule],
+  selector: 'gc-rep-fe-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BPGC Competition Reports';
+
+  ngOnInit(): void {
+    initFlowbite();
+    const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    const themeToggleLightIcon = document.getElementById(
+      'theme-toggle-light-icon',
+    );
+
+    // Change the icons inside the button based on previous settings
+    if (
+      localStorage.getItem('color-theme') === 'dark' ||
+      (!('color-theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      themeToggleLightIcon?.classList.remove('hidden');
+    } else {
+      themeToggleDarkIcon?.classList.remove('hidden');
+    }
+
+    const themeToggleBtn = document.getElementById('theme-toggle');
+
+    themeToggleBtn?.addEventListener('click', function () {
+      // toggle icons inside button
+      themeToggleDarkIcon?.classList.toggle('hidden');
+      themeToggleLightIcon?.classList.toggle('hidden');
+
+      // if set via local storage previously
+      if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+      } else {
+        if (document.documentElement.classList.contains('dark')) {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('color-theme', 'light');
+        } else {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('color-theme', 'dark');
+        }
+      }
+    });
+  }
 }
