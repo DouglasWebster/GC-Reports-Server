e3069e9babf0357d19a1e49cf324317625330dd2
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-08 16:04:17 +0000

    CompetitionService updated to use drizzle library.

--- apps/gc-rep-be/src/app/competition/competition.service.ts
+++ apps/gc-rep-be/src/app/competition/competition.service.ts
@@ -1,9 +1,9 @@
-import { compForm } from '../../db/schema/format.schema';
+import { compForm, competition } from '@lib/drizzle';
 import { Inject, Injectable } from '@nestjs/common';
 import { and, desc, eq } from 'drizzle-orm';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { DATABASE_CONNECTION } from '../../db/database/database-connection';
-import * as schema from '../../db/schema/competition.schema';
+import * as schema from '@lib/drizzle';
 
 @Injectable()
 export class CompetitionService {
@@ -15,14 +15,14 @@ export class CompetitionService {
   async getCompetitionList() {
     return await this.database
       .select({
-        id: schema.competition.id,
-        date: schema.competition.compDate,
+        id: competition.id,
+        date: competition.compDate,
         compFormat: compForm.title,
-        validated: schema.competition.isValid,
+        validated: competition.isValid,
       })
-      .from(schema.competition)
+      .from(competition)
       .leftJoin(compForm, eq(compForm.id, schema.competition.compFormId))
-      .orderBy(desc(schema.competition.compDate));
+      .orderBy(desc(competition.compDate));
   }
 
   async getCompetitionDetails() {
@@ -30,42 +30,42 @@ export class CompetitionService {
   }
 
   async getAllCompsCount() {
-    return await this.database.$count(schema.competition);
+    return await this.database.$count(competition);
   }
 
   async getCompsToReviewCount() {
     return await this.database.$count(
-      schema.competition,
-      eq(schema.competition.isValid, false)
+      competition,
+      eq(competition.isValid, false)
     );
   }
 
-  async createNamedComp(record: schema.NewCompetion) {
+  async createNamedComp(record: schema.InsertCompetion) {
     const competitionId = await this.database
       .select({
-        date: schema.competition.compDate,
-        formatId: schema.competition.compFormId,
+        date: competition.compDate,
+        formatId: competition.compFormId,
       })
-      .from(schema.competition)
+      .from(competition)
       .where(
         and(
-          eq(schema.competition.compDate, record.compDate),
-          eq(schema.competition.compFormId, record.compFormId)
+          eq(competition.compDate, record.compDate),
+          eq(competition.compFormId, record.compFormId)
         )
       );
 
     if (competitionId.length !== 0) return undefined;
     return await this.database
-      .insert(schema.competition)
+      .insert(competition)
       .values(record)
-      .returning({ competitionId: schema.competition.id });
+      .returning({ competitionId: competition.id });
   }
 
   async getCompetitionFromFormatIdAndDate(formatId: number, date: Date) {
     return await this.database.query.competition.findFirst({
       where: and(
-        eq(schema.competition.compFormId, formatId),
-        eq(schema.competition.compDate, date)
+        eq(competition.compFormId, formatId),
+        eq(competition.compDate, date)
       ),
     });
   }
