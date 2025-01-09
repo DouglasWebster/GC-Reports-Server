ee17341ef69b2a1e62df2e5652f673b92c366b6d
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-09 17:52:58 +0000

    listing unreviewed comp headers in a table.

--- apps/gc-rep-fe/src/app/review/review.component.ts
+++ apps/gc-rep-fe/src/app/review/review.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { ICompetitionGeneral } from '@libs/models';
+import { DbAccessService } from '../db-access/db-access.service';
 
 @Component({
   selector: 'gc-rep-fe-review',
@@ -7,4 +10,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './review.component.html',
   styleUrl: './review.component.css',
 })
-export class ReviewComponent {}
+export class ReviewComponent implements OnInit{
+unreviewedComps$!: Observable<ICompetitionGeneral[]>
+
+constructor(private readonly dbAccessService: DbAccessService)  {}
+
+ngOnInit(): void {
+    this.unreviewedComps$ = this.dbAccessService.getCompUnreviewedHeaders()
+}
+}
