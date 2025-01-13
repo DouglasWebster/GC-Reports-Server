86db50ae35fafd42fa5fb43e50a07e3ab57037aa
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-13 10:40:44 +0000

    Basic selection of competition to review implemented.

--- apps/gc-rep-fe/src/app/review/review.component.ts
+++ apps/gc-rep-fe/src/app/review/review.component.ts
@@ -1,32 +1,42 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { FormsModule } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { SelectCompetion, SelectPlayer } from '@libs/drizzle';
 import { ICompetitionGeneral } from '@libs/models';
 import { DbAccessService } from '../db-access/db-access.service';
 
 @Component({
   selector: 'gc-rep-fe-review',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.css',
 })
 export class ReviewComponent implements OnInit {
-  unreviewedComps$!: Observable<ICompetitionGeneral[]>;
-  reviewIds: number [] = []
-  reviewId: number | null = null
+  unreviewedComps$!: ICompetitionGeneral[];
+  compId: number | null = null;
+  compToReview: SelectCompetion | null = null;
+  compPlayers: SelectPlayer[] = [];
 
   constructor(private readonly dbAccessService: DbAccessService) {}
 
   ngOnInit(): void {
-    this.unreviewedComps$ = this.dbAccessService.getCompUnreviewedHeaders()
-    this.unreviewedComps$.subscribe((items) => {
-      for (const item of items) {
-        this.reviewIds.push(item.id)
-      }
-    })
+    this.dbAccessService
+      .getCompUnreviewedHeaders()
+      .subscribe((competition) => (this.unreviewedComps$ = competition));
   }
 
   reviewBtnClicked(id: number) {
-      this.reviewId = this.reviewIds[id];
+    this.compId = this.unreviewedComps$[id].id;
+    this.dbAccessService
+      .getCompetitionDetailsById(this.compId)
+      .subscribe((competion) => {
+        this.compToReview = competion;
+        console.log(this.compToReview);
+      });
+    this.dbAccessService
+      .getPlayersInCompetition(this.compId)
+      .subscribe((players) => {
+        for (const player of players) this.compPlayers.push(player);
+      });
   }
 }
