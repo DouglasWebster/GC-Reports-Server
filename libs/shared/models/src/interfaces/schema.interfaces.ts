f85a30dba01c04740fde9403f7d10fc0fa7d0166
Douglas Webster <douglas.webster2107@gmail.com>
2025-01-26 13:04:32 +0000

    schema now recieves the competition format title

--- libs/shared/models/src/interfaces/schema.interfaces.ts
+++ libs/shared/models/src/interfaces/schema.interfaces.ts
@@ -1,13 +1,26 @@
 export interface ICompetitionGeneral {
-    id: number,
-    date: Date,
-    compFormat: string,
-    validated: boolean
+  id: number;
+  date: Date;
+  compFormat: string;
+  validated: boolean;
 }
 
 export interface IReviewHeader {
-    compFormat: string;
-    date: string;
-    validated: boolean;
-    id: number;
+  compFormat: string;
+  date: string;
+  validated: boolean;
+  id: number;
+}
+
+export interface ICompetitionWithFormat {
+  id: number;
+  compDate: Date;
+  format: string;
+  computerEntries: number;
+  sheetEntries: number;
+  playerCount: number;
+  twosEntered: number;
+  compForm: {
+    title: string
   }
+}
