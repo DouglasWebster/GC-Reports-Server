7325151dc015ac0829eebe4306327a652d8c09fc
Douglas Webster <douglas.webster2107@gmail.com>
2025-02-01 11:39:46 +0000

    updated drizzle config to account for change in paths.

--- apps/gc-rep-be/src/drizzle.config.ts
+++ apps/gc-rep-be/src/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 import env from './utils/env'
 
 export default defineConfig({
-  schema: '../../../libs/shared/drizzle/src/index.ts',
+  schema: '../../libs/shared/drizzle/src/index.ts',
   out: './drizzle',
   dialect: 'postgresql',
   casing: "snake_case",
