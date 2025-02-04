fb4a537f9ad21e8b15189a496281ad31e2840016
Douglas Webster <douglas.webster2107@gmail.com>
2025-02-04 09:10:34 +0000

    Determine the range of years for the competitions.

--- apps/gc-rep-fe/src/app/home/home.component.ts
+++ apps/gc-rep-fe/src/app/home/home.component.ts
@@ -10,24 +10,37 @@ import { Observable } from 'rxjs';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-  noOfCompsInDb$!: Observable<number>
-  noOfMemersInDb$!: Observable<number>
-  noOfCompsToReview$!: Observable<number>
+  noOfCompsInDb$!: Observable<number>;
+  noOfMemersInDb$!: Observable<number>;
+  noOfCompsToReview$!: Observable<number>;
+  compYears: number[] = [];
 
   constructor(private readonly dbAccessService: DbAccessService) {}
 
   ngOnInit(): void {
-      this.noOfCompsInDb$ = this.dbAccessService.countAllComps()
-      this.noOfCompsToReview$ = this.dbAccessService.countCompsToReview()
-      this.noOfMemersInDb$ = this.dbAccessService.countMembers()
+    this.noOfCompsInDb$ = this.dbAccessService.countAllComps();
+    this.noOfCompsToReview$ = this.dbAccessService.countCompsToReview();
+    this.noOfMemersInDb$ = this.dbAccessService.countMembers();
+    this.calculateYearRanges();
   }
 
   updateData() {
-    this.noOfCompsInDb$ = this.dbAccessService.countAllComps()
-      this.noOfCompsToReview$ = this.dbAccessService.countCompsToReview()
-      this.noOfMemersInDb$ = this.dbAccessService.countMembers()
+    this.noOfCompsInDb$ = this.dbAccessService.countAllComps();
+    this.noOfCompsToReview$ = this.dbAccessService.countCompsToReview();
+    this.noOfMemersInDb$ = this.dbAccessService.countMembers();
   }
 
-
-
+  calculateYearRanges() {
+    let earliestCompYear = Number.MAX_SAFE_INTEGER;
+    let latestCompYear = Number.MIN_SAFE_INTEGER;
+    this.dbAccessService.getCompReviewedHeaders().subscribe((headers) => {
+      for (const header of headers) {
+        const compYear = new Date(header.date).getFullYear();
+        if (compYear < earliestCompYear) earliestCompYear = compYear;
+        if (compYear > latestCompYear) latestCompYear = compYear;
+      }
+      for (let year = earliestCompYear; year <= latestCompYear; year++)
+        this.compYears.push(year);
+    });
+  }
 }
