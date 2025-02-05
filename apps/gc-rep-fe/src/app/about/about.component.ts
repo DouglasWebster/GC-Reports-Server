9c71bc18c0096399afb2b929d9b1d29f2cc1c017
Douglas Webster <douglas.webster2107@gmail.com>
2025-02-05 08:33:10 +0000

    moved result reporting from home component to results component

--- apps/gc-rep-fe/src/app/about/about.component.ts
+++ apps/gc-rep-fe/src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'gc-rep-fe-about',
