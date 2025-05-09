1499e3ab79ef6e35740e7364dcd8cdd15a008648
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-09 09:10:47 +0100

    moved project gc-rep-be-e2e to server-e2e following move of server project

--- apps/server-e2e/jest.config.ts
+++ apps/server-e2e/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'gc-rep-be-e2e',
+  displayName: 'server-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
