df6a556c944ff3631cb53e0cf6fadf51794727e1
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-13 09:25:35 +0100

    Added auth guards to routes that are not allowed until logged in.

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -7,15 +7,16 @@ import { AboutComponent } from './about/about.component';
 import { PrintLayoutComponent } from './print-layout/print-layout.component';
 import { PayoutSheetComponent } from './payout-sheet/payout-sheet.component';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from '@lib/client/data-acess';
 
 export const appRoutes: Route[] = [
   {
     path: 'import-comp',
-    component: ImportDataComponent,
+    component: ImportDataComponent, canActivate:[authGuard]
   },
   { path: 'home', component: HomeComponent },
-  { path: 'review', component: ReviewCompComponent },
-  { path: 'results', component: ResultsComponent },
+  { path: 'review', component: ReviewCompComponent, canActivate:[authGuard] },
+  { path: 'results', component: ResultsComponent, canActivate:[authGuard] },
   { path: 'about', component: AboutComponent },
   {
     path: 'print',
