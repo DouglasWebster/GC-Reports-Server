6cb1350260fa21b71d53336ed8837c3746afa927
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-13 07:50:44 +0100

    User can go to login from home screen.

--- apps/client/src/app/home/home.component.ts
+++ apps/client/src/app/home/home.component.ts
@@ -1,20 +1,26 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core'
 import { Observable } from 'rxjs';
 import { DbAccessService } from '../db-access/db-access.service';
+import { AuthService } from '@lib/client/data-acess';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'client-home',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   private readonly dbAccessService = inject(DbAccessService);
+  private readonly authService = inject(AuthService)
 
   noOfCompsInDb$!: Observable<number>;
   noOfMemersInDb$!: Observable<number>;
   noOfCompsToReview$!: Observable<number>;
+
+  isSignedIn = this.authService.isTokenExpired()
+  login = "/login";
 
   ngOnInit(): void {
     this.updateData();
