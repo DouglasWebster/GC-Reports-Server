2dad07797a96e479b5d95327cacd12053f49ee48
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-16 10:29:32 +0100

    name added to mockUser to match update in database

--- libs/shared/mocks/src/lib/user.factory.ts
+++ libs/shared/mocks/src/lib/user.factory.ts
@@ -1,13 +1,15 @@
 import { IUser } from '@lib/shared/models';
-import { randNumber, randPassword, randUser } from '@ngneat/falso';
+import { randFullName, randNumber, randPassword, randUser } from '@ngneat/falso';
 
 export const createMockUser = (): IUser => {
     const id = randNumber({ min: 1, max: 1000 });
     const { email } = randUser();
     const password = randPassword()[0];
+    const name = randFullName({withAccents: false})
     return {
       id,
       email,
       password,
+      name,
     };
   };
