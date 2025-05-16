dcf69507515d57bba0245aee938032caaa869c01
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-16 08:07:32 +0100

    Added 2 server test suites

--- apps/server/src/app/competition/competition.controller.spec.ts
+++ apps/server/src/app/competition/competition.controller.spec.ts
@@ -1,14 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CompetitionController } from './competition.controller';
+import { CompetitionService } from './competition.service';
+import { DATABASE_CONNECTION } from '../../db/database/database-connection';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 describe('CompetitionController', () => {
   let controller: CompetitionController;
+  let service: CompetitionService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        CompetitionService,
+        {
+          provide: DATABASE_CONNECTION,
+          useValue: drizzle.mock()
+        }
+      ],
       controllers: [CompetitionController],
     }).compile();
 
+    service = module.get<CompetitionService>(CompetitionService);
     controller = module.get<CompetitionController>(CompetitionController);
   });
 
--- apps/server/src/app/tee/tee.service.spec.ts
+++ apps/server/src/app/tee/tee.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeeService } from './tee.service';
+import { DATABASE_CONNECTION } from '../../db/database/database-connection';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 describe('TeesService', () => {
   let service: TeeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TeeService],
+      providers: [TeeService,
+        {
+          provide: DATABASE_CONNECTION,
+          useValue: drizzle.mock(),
+        }
+      ],
     }).compile();
 
     service = module.get<TeeService>(TeeService);
