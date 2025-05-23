984f11040bdb6410377900d98741f2912d16f5cc
Douglas Webster <douglas.webster2107@gmail.com>
2025-05-23 10:22:45 +0100

    update result service basic test passing.

--- apps/server/src/app/update-results/update-results.service.spec.ts
+++ apps/server/src/app/update-results/update-results.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UpdateResultsService } from './update-results.service';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { MemberService } from '../member/member.service';
+import { CompFormService } from '../comp-form/comp-form.service';
+import { CompetitionService } from '../competition/competition.service';
+import { PlayerService } from '../player/player.service';
+import { TwoService } from '../two/two.service';
+import { DrizzleService } from '../../db/database/drizzle.service';
 
 describe('UpdateResultsService', () => {
   let service: UpdateResultsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UpdateResultsService],
+      providers: [
+        MemberService,
+        CompFormService,
+        CompetitionService,
+        PlayerService,
+        TwoService,
+        UpdateResultsService,
+        {
+          provide: DrizzleService,
+          useValue: drizzle.mock(),
+        },
+      ],
     }).compile();
 
     service = module.get<UpdateResultsService>(UpdateResultsService);
