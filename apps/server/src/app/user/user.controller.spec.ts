4703df01307a92be18117972c61a2e90e4c9d3ba
Douglas Webster <douglas.webster2107@gmail.com>
2025-06-05 12:12:29 +0100

    user controller tests written and user controller passing

--- apps/server/src/app/user/user.controller.spec.ts
+++ apps/server/src/app/user/user.controller.spec.ts
@@ -1,9 +1,10 @@
+import { createMockUser } from '@lib/mocks';
+import { IPublicUserData, IUser } from '@lib/shared/models';
 import { Test, TestingModule } from '@nestjs/testing';
-import { DATABASE_CONNECTION } from '../../db/database/database-connection';
+import { DrizzleService } from '../../db/database/drizzle.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { createMockUser } from '@lib/mocks';
-import { IPublicUserData } from '@lib/shared/models';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -16,7 +17,7 @@ describe('UserController', () => {
       providers: [
         UserService,
         {
-          provide: DATABASE_CONNECTION,
+          provide: DrizzleService,
           useValue: {
             db: {
               query: {
@@ -46,12 +47,12 @@ describe('UserController', () => {
     const publicUser: IPublicUserData = {
       id: user.id,
       email: user.email,
-      name: user.name
+      name: user.name,
     };
     jest
       .spyOn(service, 'create')
       .mockReturnValue(
-        Promise.resolve([{ email: user.email, userId: user.id, name: user.name }])
+        Promise.resolve({ email: user.email, userId: user.id, name: user.name })
       );
     const res = await controller.createUser({
       email: user.email,
@@ -61,15 +62,36 @@ describe('UserController', () => {
     expect(res).toStrictEqual(publicUser);
   });
 
-  it('should get user details if authenticated', async () => {
-    const user = createMockUser();
-    const publicUser: IPublicUserData = {
-      id: user.id,
-      email: user.email,
-      name: user.name
-    };
-    jest.spyOn(service, 'getOne').mockReturnValue(Promise.resolve(user));
-    const res = await controller.getUser(user.id, user.id);
-    expect(res).toStrictEqual(publicUser);
+  describe('when the GET /user/id is called', () => {
+    describe('and the requester is authenticated', () => {
+      let user1: IUser;
+      let user2: IUser;
+      beforeEach(() => {
+        user1 = createMockUser();
+        user2 = createMockUser();
+      });
+      describe('and the user with the given id exists', () => {
+        it('should respond with the user details', async () => {
+          const publicUser: IPublicUserData = {
+            id: user1.id,
+            email: user1.email,
+            name: user1.name,
+          };
+          jest.spyOn(service, 'getOne').mockReturnValue(Promise.resolve(user1));
+          const res = await controller.getUser(user1.id, publicUser.id);
+          expect(res).toStrictEqual(publicUser);
+        });
+      });
+      describe('and the user tries to access other user details', () => {
+        it('should respond with a NotFoundException sayin status code 404 Message Not Found ', async () => {
+          try {
+          await controller.getUser(user1.id, user2.id);
+          }
+          catch(err) {
+            expect(err).toBeInstanceOf(NotFoundException)
+          }
+        });
+      });
+    });
   });
 });
